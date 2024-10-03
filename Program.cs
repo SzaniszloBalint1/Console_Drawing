9d0bf4ab60dadf46d96ddab28de5ea2501505df0
Balint <szaniszlo.balint@blathy.info>
2024-10-03 16:15:41 +0200

    ad

--- Program.cs
+++ Program.cs
@@ -1,50 +1,82 @@
 ﻿using System.Drawing;
-
+using static System.Console;
 namespace Console_Drawing
 {
     internal class Program
     {
-        static void Letrehozas()
+        static void DrawMenu(int selected)
         {
+            string[] options = { "Létrehozás", "Szerkesztés", "Törlés", "Kilépés" };
             int menuWidth = 30;
             int menuHeight = 1;
             int x = (Console.WindowWidth - menuWidth) / 2;
-            int y = (Console.WindowHeight - menuHeight) / 2;
+            int y = (Console.WindowHeight - menuHeight) / 2 - 8;
 
-            DrawBox(x, y-8, menuWidth, menuHeight);
-            CenterText(y -7, "Létrehozás:", menuWidth, x);
+            for (int i = 0; i < options.Length; i++)
+            {
+                int currentY = y + i * 5;
+                DrawBox(x, currentY, menuWidth, menuHeight);
+                if (i + 1 == selected)
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                CenterText(currentY + 1, options[i] + ":", menuWidth, x);
+            }
+
+            Console.ResetColor();
         }
-        static void Szerkeztes()
+
+        static void UpdateMenuSelection(int previous, int current)
         {
+            string[] options = { "Létrehozás", "Szerkesztés", "Törlés", "Kilépés" };
             int menuWidth = 30;
             int menuHeight = 1;
             int x = (Console.WindowWidth - menuWidth) / 2;
-            int y = (Console.WindowHeight - menuHeight) / 2;
+            int y = (Console.WindowHeight - menuHeight) / 2 - 8;
 
-            DrawBox(x, y-3, menuWidth, menuHeight);
-            CenterText(y-2, "Szerkesztés:", menuWidth, x);
-           
+
+            if (previous > 0)
+            {
+                int previousY = y + (previous - 1) * 5;
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Black;
+                CenterText(previousY + 1, options[previous - 1] + ":", menuWidth, x);
+            }
+
+
+            int currentY = y + (current - 1) * 5;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            CenterText(currentY + 1, options[current - 1] + ":", menuWidth, x);
+
+            Console.ResetColor();
         }
-        static void Torles()
-        {
-            int menuWidth = 30;
-            int menuHeight = 1;
-            int x = (Console.WindowWidth - menuWidth) / 2;
-            int y = (Console.WindowHeight - menuHeight) / 2;
 
-            DrawBox(x, y+2 , menuWidth, menuHeight);
-            CenterText(y + 3, "Törlés:", menuWidth, x);
-        }
-        static void Kilepes()
+        static void ExecuteOption(int selected)
         {
-            int menuWidth = 30;
-            int menuHeight = 1;
-            int x = (Console.WindowWidth - menuWidth) / 2;
-            int y = (Console.WindowHeight - menuHeight) / 2;
-
-            DrawBox(x, y+7 , menuWidth, menuHeight);
-            CenterText(y+8, "Kilepes:", menuWidth, x);
-            
+            Console.Clear();
+            switch (selected)
+            {
+                case 1:
+                    Console.WriteLine("Létrehozás kiválasztva.");
+                    break;
+                case 2:
+                    Console.WriteLine("Szerkesztés kiválasztva.");
+                    break;
+                case 3:
+                    Console.WriteLine("Törlés kiválasztva.");
+                    break;
+                case 4:
+                    Console.WriteLine("Kilépés kiválasztva.");
+                    Environment.Exit(0);
+                    break;
+            }
+            Console.WriteLine("Nyomj meg egy gombot a visszatéréshez...");
+            Console.ReadKey();
         }
 
         static void DrawBox(int x, int y, int width, int height)
@@ -62,19 +94,64 @@ namespace Console_Drawing
             Console.Write("└" + new string('─', width) + "┘");
         }
 
-                           
-
-
         static void CenterText(int y, string text, int containerWidth, int containerX)
         {
             int textX = containerX + (containerWidth - text.Length) / 2;
             Console.SetCursorPosition(textX, y);
             Console.Write(text);
-            
         }
+
+
         static void Main()
         {
-            int cursorX = Console.WindowWidth / 2, cursorY = Console.WindowHeight / 2;
+
+
+            Console.CursorVisible = false;
+            int currentSelection = 1;
+            int previousSelection = -1;
+
+            DrawMenu(currentSelection);
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        previousSelection = currentSelection;
+                        currentSelection--;
+                        if (currentSelection < 1)
+                        {
+                            currentSelection = 4;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        previousSelection = currentSelection;
+                        currentSelection++;
+                        if (currentSelection > 4)
+                        {
+                            currentSelection = 1;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        ExecuteOption(currentSelection);
+                        DrawMenu(currentSelection);
+                        break;
+                    case ConsoleKey.Escape:
+                        break;
+                }
+
+                if (currentSelection != previousSelection)
+                {
+                    UpdateMenuSelection(previousSelection, currentSelection);
+                }
+
+
+
+
+                /*
+                 *  int cursorX = Console.WindowWidth / 2, cursorY = Console.WindowHeight / 2;
             int directionX = 0, directionY = 0;
             char currentChar = '█';
             ConsoleColor currentColor = ConsoleColor.White;
@@ -142,29 +219,28 @@ namespace Console_Drawing
 
                             switch (menuKey.Key)
                             {
-                                case ConsoleKey.UpArrow:
-                                    var choose = Console.ReadKey(true);
-
-                                    switch (choose.Key)
+                               case ConsoleKey.UpArrow:
+                                    for(int i = 0; i <directionY; i++)
+                                    if (directionY==0)
                                     {
-                                        
+                                        Szerkeztes();
+                                        Console.ForegroundColor= ConsoleColor.Yellow;
                                     }
                                    
-                                    var colorKey = Console.ReadKey(true);
-                                    switch (colorKey.Key)
+                                    if(directionY==1)
                                     {
-                                        case ConsoleKey.D1:
-                                            currentColor = ConsoleColor.White;
-                                            break;
-                                        case ConsoleKey.D2:
-                                            currentColor = ConsoleColor.Red;
-                                            break;
-                                        case ConsoleKey.D3:
-                                            currentColor = ConsoleColor.Blue;
-                                            break;
-                                        case ConsoleKey.D4:
-                                            currentColor = ConsoleColor.Green;
-                                            break;
+                                        Letrehozas();
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        directionY++;
+                                    }
+                                    break;
+
+                                    case ConsoleKey.DownArrow:
+                                    if (directionY == 1)
+                                    {
+                                        directionY--;
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Szerkeztes() ;
                                     }
                                     break;
                             }
@@ -176,6 +252,12 @@ namespace Console_Drawing
             }
 
             Console.Clear();
+                */
+
+
+            }
         }
-        }
+            
+        
+    }
 }
